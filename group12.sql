61cad86b5d6439317eb6648c509fea96e51cf316
pangPython <1253973173@qq.com>
2016-12-30 08:40:58 +0800

    add group12.sql

---
+++ group12.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.47 : Database - group12
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`group12` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `group12`;
+
+/*Table structure for table `news` */
+
+DROP TABLE IF EXISTS `news`;
+
+CREATE TABLE `news` (
+  `news_id` int(255) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `create_date` date NOT NULL,
+  `update_date` date NOT NULL,
+  PRIMARY KEY (`news_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `news` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(255) NOT NULL AUTO_INCREMENT,
+  `pwd` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_sex` int(10) NOT NULL,
+  `tel` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`user_id`,`pwd`,`user_name`,`user_sex`,`tel`,`email`) values (1,'123456','pangPython',1,'15762180000','pangpython@qq.com'),(2,'admin','chengrui',0,'15762185555','cr@ali.com'),(3,'123','xuhuancai',0,'15762182121','xhc@tencent.com'),(4,'4321','dupeng',1,'15762185555','dp@baidu.com');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
