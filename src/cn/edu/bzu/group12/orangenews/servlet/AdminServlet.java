0db842821a26f1b33d5aeca86296d216bfc64a84
pangPython <1253973173@qq.com>
2017-01-05 10:37:19 +0800

    add amazeUI admin module

---
+++ src/cn/edu/bzu/group12/orangenews/servlet/AdminServlet.java
@@ -0,0 +1,29 @@
+package cn.edu.bzu.group12.orangenews.servlet;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * @author pangPython
+ *	ºóÌ¨servlet
+ */
+public class AdminServlet extends HttpServlet {
+
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		//¼ì²âµÇÂ¼×´Ì¬
+		req.getRequestDispatcher("admin/index.jsp").forward(req, res);
+		//Ìø×ªÒ³Ãæ
+	}
+	
+}
